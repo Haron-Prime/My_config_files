924df4d783e65928339caee76be595b0ba5ca824
Haron-Prime <haron.prime@gmx.com>
2016-10-14 13:30:23 +0300

    Update

--- .xmobarrc.hs
+++ .xmobarrc.hs
@@ -17,17 +17,18 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
        , allDesktops = True
        , overrideRedirect = True
        , commands = [ Run XMonadLog
-                    , Run Com "root-disk" [] "" 100
                     , Run Network "enp3s0" ["-S", "True", "-d", "1", "-t", "<fc=red>↓</fc><fn=2> </fn><rx> <fc=green>↑</fc><fn=2> </fn><tx>"] 10
                     , Run Com "Net_in" [] "" 600
                     , Run Com "Net_out" [] "" 600
-                    , Run Cpu ["-t","CPU:<fn=2> </fn><total>%","-L","10","-H","75","--normal","yellow","--high","orange"] 10
-                    , Run Com "cpu-temp" [] "" 30
+                    , Run Cpu ["-t","CPU:<fn=2> </fn><total>%","-L","25","-H","75","--normal","yellow","--high","orange"] 10
+                    , Run Com "cpu-temp" [] "" 20
                     , Run Com "cpu-freq" [] "" 100
-                    , Run Com "mb-temp" [] "" 30
+                    , Run Com "mb-temp" [] "" 20
                     --, Run Com "fan1" [] "" 50
                     --, Run Com "fan2" [] "" 50
-                    , Run Memory ["-t", "Mem:<fn=2> </fn><usedratio>%","-L","50","-H","90","--normal","yellow","--high","orange"] 50
+                    , Run Memory ["-t", "RAM:<fn=2> </fn><usedratio>%","-L","50","-H","90","--normal","yellow","--high","orange"] 50
+                    , Run Com "root-disk2" [] "" 100
+                    , Run Com "x-uptime" [] "" 600
                     --, Run Volume "default" "Master" [] 10
                     , Run Com "vol2.sh" [] "" 10
                     , Run Kbd [("us", "<fc=#aaddff>EN</fc>"), ("ru", "<fc=orange>RU</fc>")]
@@ -39,5 +40,5 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc><  %root-disk%  %enp3s0%  <fc=red>↓</fc><fn=2> </fn>%Net_in% <fc=green>↑</fc><fn=2> </fn>%Net_out%  %cpu% %cpu-temp% %cpu-freq%MHz  MB:<fn=2> </fn>%mb-temp%  %memory%  Vol:<fn=2> </fn>%vol2.sh%  <fn=1>%kbd%</fn>  %xmdate% <fc=#aaddff>%xmtime%</fc>   <fn=1><fc=#aaddff>%weather%</fc></fn>"
+       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc><  %enp3s0%  <fc=red>↓</fc><fn=2> </fn>%Net_in% <fc=green>↑</fc><fn=2> </fn>%Net_out%  %cpu% %cpu-temp% %cpu-freq%MHz  MB:<fn=2> </fn>%mb-temp%  %memory%  ROM:<fn=2> </fn>%root-disk2%  Up:<fn=2> </fn>%x-uptime%h  Vol:<fn=2> </fn>%vol2.sh%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>   <fn=1><fc=#aaddff>%weather%</fc></fn>"
        }
