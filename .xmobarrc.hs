7d1c0ca0368e3d88a6f2d6108005874dc6bd7537
Haron-Prime <haron.prime@gmx.com>
2016-10-15 11:59:22 +0300

    Update

--- .xmobarrc.hs
+++ .xmobarrc.hs
@@ -17,7 +17,7 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
        , allDesktops = True
        , overrideRedirect = True
        , commands = [ Run XMonadLog
-                    , Run Network "enp3s0" ["-S", "True", "-d", "1", "-t", "<fc=red>↓</fc><fn=2> </fn><rx> <fc=green>↑</fc><fn=2> </fn><tx>"] 10
+                    , Run Network "enp3s0" ["-S", "True", "-d", "1", "-t", "<rx><fn=2> </fn><fc=green>↓</fc><fc=red>↑</fc><fn=2> </fn><tx>"] 10
                     , Run Com "Net_in" [] "" 600
                     , Run Com "Net_out" [] "" 600
                     , Run Cpu ["-t","CPU:<fn=2> </fn><total>%","-L","25","-H","75","--normal","yellow","--high","orange"] 10
@@ -40,5 +40,5 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc><  %enp3s0%  <fc=red>↓</fc><fn=2> </fn>%Net_in% <fc=green>↑</fc><fn=2> </fn>%Net_out%  %cpu% %cpu-temp% %cpu-freq%MHz  MB:<fn=2> </fn>%mb-temp%  %memory%  ROM:<fn=2> </fn>%root-disk2%  Up:<fn=2> </fn>%xmuptime%  Vol:<fn=2> </fn>%vol2.sh%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>   <fn=1><fc=#aaddff>%weather%</fc></fn>"
+       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc><  %enp3s0%  %Net_in%<fn=2> </fn><fc=green>↓</fc><fc=red>↑</fc><fn=2> </fn>%Net_out%  %cpu% %cpu-temp% %cpu-freq%MHz  MB:<fn=2> </fn>%mb-temp%  %memory%  ROM:<fn=2> </fn>%root-disk2%  Up:<fn=2> </fn>%xmuptime%  Vol:<fn=2> </fn>%vol2.sh%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>   <fn=1><fc=#aaddff>%weather%</fc></fn>"
        }
