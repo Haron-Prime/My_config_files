5a0eccbeb29b971c517e15454deee9f47bb1a441
Haron-Prime <haron.prime@gmx.com>
2016-10-15 11:24:22 +0300

    Update

--- .xmobarrc.hs
+++ .xmobarrc.hs
@@ -28,7 +28,7 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
                     --, Run Com "fan2" [] "" 50
                     , Run Memory ["-t", "RAM:<fn=2> </fn><usedratio>%","-L","50","-H","90","--normal","yellow","--high","orange"] 50
                     , Run Com "root-disk2" [] "" 100
-                    , Run Com "x-uptime" [] "" 600
+                    , Run Com "xmuptime" [] "" 600
                     --, Run Volume "default" "Master" [] 10
                     , Run Com "vol2.sh" [] "" 10
                     , Run Kbd [("us", "<fc=#aaddff>EN</fc>"), ("ru", "<fc=orange>RU</fc>")]
@@ -40,5 +40,5 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc><  %enp3s0%  <fc=red>↓</fc><fn=2> </fn>%Net_in% <fc=green>↑</fc><fn=2> </fn>%Net_out%  %cpu% %cpu-temp% %cpu-freq%MHz  MB:<fn=2> </fn>%mb-temp%  %memory%  ROM:<fn=2> </fn>%root-disk2%  Up:<fn=2> </fn>%x-uptime%h  Vol:<fn=2> </fn>%vol2.sh%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>   <fn=1><fc=#aaddff>%weather%</fc></fn>"
+       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc><  %enp3s0%  <fc=red>↓</fc><fn=2> </fn>%Net_in% <fc=green>↑</fc><fn=2> </fn>%Net_out%  %cpu% %cpu-temp% %cpu-freq%MHz  MB:<fn=2> </fn>%mb-temp%  %memory%  ROM:<fn=2> </fn>%root-disk2%  Up:<fn=2> </fn>%xmuptime%  Vol:<fn=2> </fn>%vol2.sh%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>   <fn=1><fc=#aaddff>%weather%</fc></fn>"
        }
