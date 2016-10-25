9dd811045207ac739271847ec5f369fbc603a23f
Haron-Prime <haron.prime@gmx.com>
2016-10-25 17:41:50 +0300

    Update

--- .xmobarrc.hs
+++ .xmobarrc.hs
@@ -25,6 +25,8 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
                     -- , Run Com "cpu-freq" [] "" 100
                     , Run Com "TMB" [] "" 10
                     , Run Com "TGPU" [] "" 10
+                    , Run Com "fan1" [] "" 30
+                    , Run Com "fan2" [] "" 30
                     , Run Memory ["-t", "RAM:<fn=2> </fn><usedratio>%","-L","50","-H","85","--normal","yellow","--high","orange"] 50
                     , Run Com "ROM" [] "" 100
                     , Run Com "xmuptime" [] "" 600
@@ -36,5 +38,5 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc>< %enp3s0%  %Net_in%<fn=2> </fn><fc=green>↓</fc><fc=red>↑</fc><fn=2> </fn>%Net_out%  %cpu% %TCPU%  MB:<fn=2> </fn>%TMB%  GPU:<fn=2> </fn>%TGPU%  %memory%  ROM:<fn=2> </fn>%ROM%  Up:<fn=2> </fn>%xmuptime%  Vol:<fn=2> </fn>%XVol%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>  <fn=1><fc=#aaddff>%weather%</fc></fn>"
+       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc>< %enp3s0%  %Net_in%<fn=2> </fn><fc=green>↓</fc><fc=red>↑</fc><fn=2> </fn>%Net_out%  %cpu% %TCPU%  MB:<fn=2> </fn>%TMB%  GPU:<fn=2> </fn>%TGPU%  FAN:<fn=2> </fn>%fan1% %fan2%  %memory%  ROM:<fn=2> </fn>%ROM%  Up:<fn=2> </fn>%xmuptime%  Vol:<fn=2> </fn>%XVol%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>  <fn=1><fc=#aaddff>%weather%</fc></fn>"
        }
