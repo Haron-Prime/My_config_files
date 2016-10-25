cb3fccfc36523b4bb81a8c3f11108747008a7ebb
Haron-Prime <haron.prime@gmx.com>
2016-10-25 12:15:12 +0300

    Update

--- .xmobarrc.hs
+++ .xmobarrc.hs
@@ -1,10 +1,10 @@
 Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
        , additionalFonts = ["xft:Terminus:size=12:weight=bold:antialias=true:hinting=true","xft:Terminus Re33:size=4:antialias=true:hinting=true"] 
        -- , borderColor = "#151515"
-       -- , border = TopB
+       , border = NoBorder
        , bgColor = "#151515"
        , fgColor = "#959595"
-       , alpha = 0
+       , alpha = 1
        --, position = Static { xpos = 0, ypos = 0, width = 1850, height = 20 }
        , position = TopP 0 66
        , textOffset = -1
@@ -24,6 +24,7 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
                     , Run Com "TCpu" [] "" 10
                     -- , Run Com "cpu-freq" [] "" 100
                     , Run Com "TMB" [] "" 10
+                    , Run Com "TNV" [] "" 10
                     , Run Memory ["-t", "RAM:<fn=2> </fn><usedratio>%","-L","50","-H","85","--normal","yellow","--high","orange"] 50
                     , Run Com "ROM" [] "" 100
                     , Run Com "xmuptime" [] "" 600
@@ -35,5 +36,5 @@ Config { font = "xft:Terminus Re33:size=12:antialias=true:hinting=true"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc><  %enp3s0%  %Net_in%<fn=2> </fn><fc=green>↓</fc><fc=red>↑</fc><fn=2> </fn>%Net_out%  %cpu% %TCpu%  MB:<fn=2> </fn>%TMB%  %memory%  ROM:<fn=2> </fn>%ROM%  Up:<fn=2> </fn>%xmuptime%  Vol:<fn=2> </fn>%XVol%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>   <fn=1><fc=#aaddff>%weather%</fc></fn>"
+       , template = " %XMonadLog% }{ <<fc=#88bbdd><</fc><fc=#aaddff><</fc><fc=#88bbdd><</fc><  %enp3s0%  %Net_in%<fn=2> </fn><fc=green>↓</fc><fc=red>↑</fc><fn=2> </fn>%Net_out%  %cpu% %TCpu%  MB:<fn=2> </fn>%TMB%  VC:<fn=2> </fn>%TNV%  %memory%  ROM:<fn=2> </fn>%ROM%  Up:<fn=2> </fn>%xmuptime%  Vol:<fn=2> </fn>%XVol%  <fn=1>%kbd%</fn>  %xmdate%  <fc=#aaddff>%xmtime%</fc>   <fn=1><fc=#aaddff>%weather%</fc></fn>"
        }
