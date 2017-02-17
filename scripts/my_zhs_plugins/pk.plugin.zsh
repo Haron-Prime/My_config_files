725e41be037dd34d7284c58a67013310343d2d32
Haron-Prime <haron.prime@gmx.com>
2017-02-17 18:55:34 +0200

    Update

--- scripts/my_zhs_plugins/pk.plugin.zsh
+++ scripts/my_zhs_plugins/pk.plugin.zsh
@@ -5,8 +5,8 @@ pk () {
   case $1 in
   tbz)       tar cjvf $2.tar.bz2 $2      ;;
   tgz)       tar czvf $2.tar.gz  $2       ;;
-  tar)      tar cpvf $2.tar  $2       ;;
-  bz2)    bzip $2 ;;
+  tar)       tar cpvf $2.tar  $2       ;;
+  bz2)       bzip $2 ;;
   gz)        gzip -c -9 -n $2 > $2.gz ;;
   zip)       zip -r $2.zip $2   ;;
   7z)        7z a $2.7z $2    ;;
