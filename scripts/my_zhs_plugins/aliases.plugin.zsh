d3fea96387c623d84f55f7b2e8ba767892f47a12
Haron-Prime <haron.prime@gmx.com>
2017-02-22 13:36:09 +0200

    Update

--- scripts/my_zhs_plugins/aliases.plugin.zsh
+++ scripts/my_zhs_plugins/aliases.plugin.zsh
@@ -8,6 +8,8 @@ alias x='exit'
 alias ipt='iptables -L -vn'
 alias dump='sudo screendump > ~/output.txt'
 # alias gmx='mount https://storage-file-eu.gmx.com'
+alias nout='ssh haron@192.168.0.11 -p 232'
+alias tv='mpv ~/Video/multinet-play.m3u'
 alias st='subl3'
 alias sst='sudo subl3'
 alias vnspeed='vnstat -l -i enp3s0'
@@ -46,15 +48,85 @@ alias xx='startx ~/.xinitrc-xmonad'
 alias xh='startx ~/.xinitrc-herbstluftwm'
 alias xi='startx ~/.xinitrc-i3'
 alias xa='startx ~/.xinitrc-awesome'
-alias xs='startx ~/.xinitrc-spectrwm'
 alias gitf='git add --all && git commit -m "Update" && git push'
 alias gita='git add'
 alias giti='git add -i'
 alias gitc='git commit -m "Update"'
 alias gitp='git push'
 alias gits='git status'
-alias nout='ssh haron@192.168.0.11 -p 232'
-alias tv='mpv ~/Video/multinet-play.m3u'
+
+
+# цветной вывод с помощью grc
+# usage grc
+if [ -f /usr/bin/grc ]; then
+  alias configure='grc --colour=auto configure'
+  alias ping="grc --colour=auto ping -4"
+  alias traceroute="grc --colour=auto traceroute"
+  alias gcc="grc --colour=auto gcc"
+  alias make="grc --colour=auto make"
+  alias netstat="grc --colour=auto netstat"
+  alias stat="grc --colour=auto stat"
+  alias ss="grc --colour=auto ss"
+  alias diff="grc --colour=auto diff"
+  alias wdiff="grc --colour=auto wdiff"
+  alias last='grc --colour=auto last'
+  alias who='grc --colour=auto who'
+  alias mount="grc --colour=auto mount"
+  alias mtr="grc --colour=auto mtr"
+  alias ps="grc --colour=auto ps"
+  alias dig="grc --colour=auto dig"
+  alias ifconfig='grc --colour=auto ifconfig'
+  alias ls="grc --colour=auto ls"
+  alias df="grc --colour=auto df"
+  alias du="grc --colour=auto du"
+  alias addr="grc --colour=auto ip addr"
+  alias link="grc --colour=auto ip link"
+  alias route="grc --colour=auto ip route"
+  alias neighbor='grc --colour=ip neighbor'
+  alias env="grc --colour=auto env"
+  alias iptables='sudo grc --colour=auto iptables'
+  alias lspci='grc --colour=auto lspci'
+  alias lsblk='grc --colour=auto lsblk'
+  alias blkid='grc --colour=auto blkid'
+  alias iostat='grc --colour=auto iostat'
+  alias sar='grc --colour=auto sar'
+  alias free='grc --colour=auto free'
+  alias journalctl='grc --colour=auto journalctl'
+  alias sysctl='grc --colour=auto sysctl'
+  alias tcpdump='grc --colour=auto tcpdump'
+  alias tune2fs='grc --colour=auto tune2fs'
+  alias lsmod='grc --colour=auto lsmod'
+  alias lsattr='grc --colour=auto lsattr'
+  alias semanage='grc --colour=auto semanage'
+  alias getsebool='grc --colour=auto getsebool'
+  alias unlimit='grc --colour=auto unlimit'
+  alias vnstat="grc --colour=auto vnstat"
+  alias dnf='grc --colour=auto dnf'
+  alias nmap='grc --colour=auto nmap'
+  alias uptime='grc --colour=auto uptime'
+  alias w='grc --colour=auto w'
+  alias getfacl='grc --colour=auto getfacl'
+  alias logc="grc --colour=auto cat"
+  alias logt="grc --colour=auto tail"
+  alias logh="grc --colour=auto head"
+
+  alias warn="grc -c conf.warn cat"
+fi
+
+# other aliases
+alias ls="ls -la --classify --color=auto"
+alias grep='grep --colour=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias li='ls -ial'
+
+alias -s {avi,mpeg,mpg,mov,m2v,mkv}=mpv
+alias -s {odt,doc,sxw,rtf}=libreoffice
+alias -s {png,gif,jpg,jpeg}=feh
+alias -s text=vim
 
 # Распаковка архивов
 # example: extract file
@@ -99,77 +171,3 @@ pk () {
  fi
 
 } 
-
-# цветной вывод с помощью grc
-# usage grc
-if [ -f /usr/bin/grc ]; then
-  alias configure='grc --colour=auto configure'
-  alias ping="grc --colour=auto ping -4"
-  alias traceroute="grc --colour=auto traceroute"
-  alias gcc="grc --colour=auto gcc"
-  alias make="grc --colour=auto make"
-  alias netstat="grc --colour=auto netstat"
-  alias stat="grc --colour=auto stat"
-  alias ss="grc --colour=auto ss"
-  alias diff="grc --colour=auto diff"
-  alias wdiff="grc --colour=auto wdiff"
-  alias last='grc --colour=auto last'
-  alias who='grc --colour=auto who'
-  alias mount="grc --colour=auto mount"
-  alias mtr="grc --colour=auto mtr"
-  alias ps="grc --colour=auto ps"
-  alias dig="grc --colour=auto dig"
-  alias ifconfig='grc --colour=auto ifconfig'
-  alias ls="grc --colour=auto ls"
-  alias df="grc --colour=auto df"
-  alias du="grc --colour=auto du"
-  alias addr="grc --colour=auto ip addr"
-  alias link="grc --colour=auto ip link"
-  alias route="grc --colour=auto ip route"
-  alias neighbor='grc --colour=ip neighbor'
-  alias env="grc --colour=auto env"
-  alias iptables='sudo grc --colour=auto iptables'
-  alias lspci='grc --colour=auto lspci'
-  alias lsblk='grc --colour=auto lsblk'
-  alias blkid='grc --colour=auto blkid'
-  alias iostat='grc --colour=auto iostat'
-  alias sar='grc --colour=auto sar'
-  alias free='grc --colour=auto free'
-
-  alias journalctl='grc --colour=auto journalctl'
-  alias sysctl='grc --colour=auto sysctl'
-  alias tcpdump='grc --colour=auto tcpdump'
-  alias tune2fs='grc --colour=auto tune2fs'
-  alias lsmod='grc --colour=auto lsmod'
-  alias lsattr='grc --colour=auto lsattr'
-  alias semanage='grc --colour=auto semanage'
-  alias getsebool='grc --colour=auto getsebool'
-  alias unlimit='grc --colour=auto unlimit'
-  alias vnstat="grc --colour=auto vnstat"
-  alias dnf='grc --colour=auto dnf'
-  alias nmap='grc --colour=auto nmap'
-  alias uptime='grc --colour=auto uptime'
-  alias w='grc --colour=auto w'
-  alias getfacl='grc --colour=auto getfacl'
-
-  alias logc="grc --colour=auto cat"
-  alias logt="grc --colour=auto tail"
-  alias logh="grc --colour=auto head"
-
-  alias warn="grc -c conf.warn cat"
-fi
-
-# aliases
-alias ls="ls -la --classify --color=auto"
-alias grep='grep --colour=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-alias li='ls -ial'
-
-alias -s {avi,mpeg,mpg,mov,m2v,mkv}=mpv
-alias -s {odt,doc,sxw,rtf}=libreoffice
-alias -s {png,gif,jpg,jpeg}=feh
-alias -s text=vim
