bedb65a87c3f7215906bc15cc7c2906e9b1a2812
Haron-Prime <haron.prime@gmx.com>
2017-03-02 19:45:14 +0200

    Update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -291,15 +291,16 @@ myLogHook = do
                                         }
 
 -- nameScratchpad
-mynameScratchpads = [ NS "ncmpcpp"      "urxvtc -name ncmpcpp -e ncmpcpp" (appName    =? "ncmpcpp")      (customFloating $ W.RationalRect 0.15 0.2 0.7 0.6)
-                    , NS "htop"         "urxvtc -name htop -e htop"       (appName    =? "htop")         (customFloating $ W.RationalRect 0.05 0.05 0.9 0.9)
-                    , NS "gpick"        "gpick"                           (appName    =? "gpick")        (customFloating $ W.RationalRect 0.2 0.2 0.6 0.6)
-                    , NS "pavucontrol"  "pavucontrol"                     (appName    =? "pavucontrol")  (customFloating $ W.RationalRect 0.2 0.2 0.6 0.6)
+mynameScratchpads = [ NS "ncmpcpp"      "urxvtc -name ncmpcpp -e ncmpcpp"     (appName    =? "ncmpcpp")      (customFloating $ W.RationalRect 0.15 0.2 0.7 0.6)
+                    , NS "htop"         "urxvtc -name htop -e htop"           (appName    =? "htop")         (customFloating $ W.RationalRect 0.05 0.05 0.9 0.9)
+                    , NS "gpick"        "gpick"                               (appName    =? "gpick")        (customFloating $ W.RationalRect 0.2 0.2 0.6 0.6)
+                    , NS "pavucontrol"  "pavucontrol"                         (appName    =? "pavucontrol")  (customFloating $ W.RationalRect 0.2 0.2 0.6 0.6)
+                    , NS "update"       "urxvtc -name update -e yaourt -Syua" (appName    =? "update")      (customFloating $ W.RationalRect 0.15 0.2 0.7 0.6)
 
-                    , NS "Mirage"       "mirage"                          (className  =? "Mirage")       (customFloating $ W.RationalRect 0.05 0.05 0.9 0.9)
-                    , NS "font-manager" "font-manager"                    (className  =? "Font-manager") (customFloating $ W.RationalRect 0.2 0.2 0.6 0.6)
+                    , NS "Mirage"       "mirage"                              (className  =? "Mirage")       (customFloating $ W.RationalRect 0.05 0.05 0.9 0.9)
+                    , NS "font-manager" "font-manager"                        (className  =? "Font-manager") (customFloating $ W.RationalRect 0.2 0.2 0.6 0.6)
 
-                    , NS "Organizer"    "Organizer"                       (role       =? "Organizer")    (customFloating $ W.RationalRect 0.1 0.1 0.8 0.8)
+                    , NS "Organizer"    "Organizer"                           (role       =? "Organizer")    (customFloating $ W.RationalRect 0.1 0.1 0.8 0.8)
                     ]
 
 -- Scratchpad
