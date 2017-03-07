7eeae6bad1869b51a1e51e2f53b2b61b3a634586
Haron-Prime <haron.prime@gmx.com>
2017-03-07 16:23:34 +0200

    Update

--- .xmonad/xmobarrc.hs
+++ .xmonad/xmobarrc.hs
@@ -10,7 +10,7 @@ Config {
        , fgColor          = "#959595"
        , alpha            = 1
        , position         = TopP 0 54
-       , textOffset       = 14
+       , textOffset       = 15
        , iconOffset       = -1
        , lowerOnStart     = True
        , pickBroadest     = True
@@ -34,12 +34,11 @@ Config {
                             , Run Com        "XMRom" [] "" 100
                             , Run Com        "XMUptime" [] "" 100
                             , Run Com        "XMVol" [] "" 10
-                            -- , Run PipeReader "/tmp/vol" "Vol"
                             , Run Kbd        [("us", "<fn=1><fc=#95d5f5>EN</fc></fn>"), ("ru", "<fn=1><fc=#ff6500>RU</fc></fn>")]
                             , Run Com        "XMTime" [] "" 10
                             , Run Com        "XMWeather" [] "" 100
                             ]
        , sepChar  = "%"
        , alignSep = "}{"
-       , template = "<action=`i3lock -i /home/haron/wall/starrynight.png` button=1><icon=arch-mono-16x16.xpm/>%XMNull%</action> %XMonadLog%}{<action=XMYaourt>%Update%%XMUpdate%</action>  <action=`XMVnstat-h` button=1><action=`XMVnstat-d` button=3>%dynnetwork%</action></action>  <action=`XMVnstat` button=1><action=`XMVnstat-m` button=3>%XMTraf%</action></action>  <action=`XMTop-cpu` button=1><action=`XMHtop` button=3>%cpu% %TCPU%</action></action>  <action=`XMSensors` button=13>%TMB%  %TGPU%</action>  <action=`XMTop-mem` button=1><action=`XMFree` button=3>%memory%</action></action>  <action=`XMdf-h` button=1><action=`XMblkid` button=3>%XMRom%</action></action>  %XMUptime%  <action=`/usr/bin/pulseaudio-ctl up` button=5><action=`/usr/bin/pulseaudio-ctl down` button=4><action=`/usr/bin/pulseaudio-ctl mute` button=1><action=`mpc toggle` button=3>%XMVol%</action></action></action></action>  <action=`xdotool key Mode_switch` button=1>%kbd%</action>  <action=`XMCal` button=1><action=`XMXClock` button=3>%XMTime%</action></action>  <action=`XMWeather-current` button=13>%XMWeather%</action>"
+       , template = "<action=`i3lock -i /home/haron/wall/starrynight.png` button=1><icon=arch-mono-16x16.xpm/>%XMNull%</action> %XMonadLog%}{<action=XMYaourt>%Update%%XMUpdate%</action>  <action=`XMVnstat-h` button=1><action=`XMVnstat-d` button=3>%dynnetwork%</action></action>  <action=`XMVnstat` button=1><action=`XMVnstat-m` button=3>%XMTraf%</action></action>  <action=`XMTop-cpu` button=1><action=`XMHtop` button=3>%cpu% %TCPU%</action></action>  <action=`XMSensors` button=13>%TMB%  %TGPU%</action>  <action=`XMTop-mem` button=1><action=`XMFree` button=3>%memory%</action></action>  <action=`XMdf-h` button=1><action=`XMblkid` button=3>%XMRom%</action></action>  %XMUptime%  <action=`/usr/bin/pulseaudio-ctl up` button=5><action=`/usr/bin/pulseaudio-ctl down` button=4><action=`/usr/bin/pulseaudio-ctl mute` button=1><action=`mpc toggle` button=3>%XMVol%</action></action></action></action>  <action=`xdotool key Mode_switch` button=1>%kbd%</action>  <action=`XMCal` button=1>%XMTime%</action>  <action=`XMWeather-current` button=13>%XMWeather%</action>"
        }
