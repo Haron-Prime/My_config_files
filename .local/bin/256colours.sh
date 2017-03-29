600ec955f753722eea5eca6242e11f11db6c0734
Haron-Prime <haron.prime@gmx.com>
2017-03-29 16:31:13 +0300

    Update

--- .local/bin/256colours.sh
+++ .local/bin/256colours.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for i in {0..255} ; do
     printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
-    if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
+    if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 15 == 0 )); then
         printf "\n";
     fi
 done
