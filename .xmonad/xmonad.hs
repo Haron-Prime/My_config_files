f8e9edcac2503a4a95cf9e45cd3e79661dbf186b
Haron-Prime <haron.prime@gmx.com>
2017-04-11 13:16:44 +0300

    Update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -352,7 +352,7 @@ myLogHook = do
                                         }
 
 -- StartupHook
-myStartupHook        =  return () <+> adjustEventInput <+> setWMName "LG3D" <+> onScr 1 W.greedyView "W" <+> spawn "echo > /tmp/haron/minwin" <+> spawn "gis-weather"
+myStartupHook        =  return () <+> adjustEventInput <+> setWMName "LG3D" <+> onScr 1 W.greedyView "W" <+> spawn "echo > /tmp/haron/minwin" <+> spawn "gis-weather" <+> spawn "stalometray"
 
 myConfig = ewmh $ withUrgencyHookC NoUrgencyHook urgencyConfig def {
                   terminal           = myTerminal
