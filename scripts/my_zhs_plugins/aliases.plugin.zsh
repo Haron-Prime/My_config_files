267870720945ada180692c32296db3ade97364aa
Haron-Prime <haron.prime@gmx.com>
2017-07-18 12:51:05 +0300

    Update

--- scripts/my_zhs_plugins/aliases.plugin.zsh
+++ scripts/my_zhs_plugins/aliases.plugin.zsh
@@ -56,6 +56,7 @@ alias sv='s -b vivaldi-snapshot'
 alias mcf='cd $HOME/git/My_config_files'
 alias bb='cd $HOME/git/BitBuket/my_config_files'
 alias gsync='rsync -ruptgo --exclude ".git" --delete --progress /home/haron/git/My_config_files /home/haron/git/BitBuket/my_config_files'
+alias ggsync='rsync -ruptgo --exclude ".git" --delete --progress /home/haron/git/My_config_files /home/haron/git/GitLab/'
 
 # alias mfs='sudo mhddfs /media/haron/BKP,/media/haron/Data /mnt/all -o default_permissions,allow_other'
 # alias gmx='mount https://storage-file-eu.gmx.com'
