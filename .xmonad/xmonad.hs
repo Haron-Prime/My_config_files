1e3a54cd600cae56bef862ddca8d3cd92492fbfe
Haron-Prime <haron.prime@gmx.com>
2017-08-02 15:27:34 +0300

    Update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -382,7 +382,7 @@ myLogHook = do
                                         }
 
 -- StartupHook
-myStartupHook  =  return () <+> adjustEventInput <+> setWMName "LG3D" <+> onScr 1 W.greedyView "W" <+> spawn "gis-weather & XMStateStart & XMUpdate & XMNetTest & XMTrans & XMMPD & XMTime2" 
+myStartupHook  =  return () <+> adjustEventInput <+> setWMName "LG3D" <+> onScr 1 W.greedyView "W" <+> spawn "gis-weather & XMStateStart & XMUpdate & XMNetTest & XMTrans & XMMPD & XMTimeState" 
 
 myConfig = ewmh $ withUrgencyHookC NoUrgencyHook urgencyConfig def {
                   terminal           = myTerminal
