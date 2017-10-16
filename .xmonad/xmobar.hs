230aa1f92ebd231b54f1b751b10b34d74122d58c
Haron Prime <haron.prime@gmx.com>
2017-10-16 23:32:52 +0300

    Update

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -2,7 +2,7 @@
 -- Author - Haron Prime
 -- License WTFPL © 2017 http://www.wtfpl.net/
 
--- List of all scripts used and their description is in the file /.local/bin/README.md
+-- List of all scripts used and their description is in the file https://github.com/Haron-Prime/My_config_files/blob/master/.local/bin/README.md
 
 Config {
          position         = Static { xpos = 0 , ypos = 0, width = 1920, height = 24 }
